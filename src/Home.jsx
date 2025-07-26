22a6bc8cde3fea3f32e780a4366b6f3b085d637d
Farhan Falak Sher <farhanfalaksher533@gmail.com>
2025-07-27 01:40:57 +0500

    fourth commit

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,8 @@
 import React from "react";
-import myImage from "./assets/Farhan.webp";
 
 const Home = () => {
   return (
-    <section id="home" className="h-[80vh] bg-cover bg-center relative" style={{ backgroundImage: `url(${myImage})` }}>
+    <section id="home" className="h-[80vh] bg-cover bg-center relative">
       <div className="absolute inset-0 bg-black bg-opacity-60 flex items-center justify-center">
         <h1 className="text-4xl md:text-6xl font-bold text-white text-center">
           Farhan Falaksher — Frontend Developer
